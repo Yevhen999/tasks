f9e03152f59d6926db20519175e44e64908a33e9
Yevhen999 <leshchenkoyevhen@gmail.com>
2022-11-05 16:54:40 +0000

    Deploying to gh-pages from @ Yevhen999/tasks@5c1bcad5b8bf48dda445f394f6dae66e0a400ec2 🚀

---
+++ index.c87378f4.js
@@ -0,0 +1,2 @@
+!function(){function o(o){var e=o.toLowerCase().replace(/\W|\_/g,""),n=e.split("").reverse().join("");return e===n}console.log(o("_eye")),console.log(o("0_0 (: /- :) 0-0")),console.log(o("five|_/|four"))}();
+//# sourceMappingURL=index.c87378f4.js.map
