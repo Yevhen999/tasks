f996eb4f4b80ec8e8b26b0257fdbad5c54f40daf
Yevhen999 <leshchenkoyevhen@gmail.com>
2022-11-05 17:15:04 +0000

    Deploying to gh-pages from @ Yevhen999/tasks@3b787ec01cb7f3ebc63fbb85d3d6a49240034ef7 🚀

--- index.063dca10.js
+++
@@ -1,2 +0,0 @@
-function o(o){const e=o.toLowerCase().replace(/\W|\_/g,""),l=e.split("").reverse().join("");return e===l}console.log(o("_eye")),console.log(o("0_0 (: /- :) 0-0")),console.log(o("five|_/|four"));
-//# sourceMappingURL=index.063dca10.js.map
--- index.c87378f4.js
+++
@@ -1,2 +0,0 @@
-!function(){function o(o){var e=o.toLowerCase().replace(/\W|\_/g,""),n=e.split("").reverse().join("");return e===n}console.log(o("_eye")),console.log(o("0_0 (: /- :) 0-0")),console.log(o("five|_/|four"))}();
-//# sourceMappingURL=index.c87378f4.js.map
