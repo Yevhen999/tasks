0f6d865ebf28bd2e06dd5a4fdf885cbc0554a4d5
Yevhen <leshchenkoyevhen@gmail.com>
2022-11-07 11:29:51 -0500

    solution convertToRoman

--- src/js/roman.js
+++ src/js/roman.js
@@ -1,1 +1,28 @@
-// qewqeqwe
+function convertToRoman(num) {
+  const dependence = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let romans = '';
+
+  for (let key in dependence) {
+    while (num >= dependence[key]) {
+      romans += key;
+      num -= dependence[key];
+    }
+  }
+  return romans;
+}
+
+convertToRoman(36);
