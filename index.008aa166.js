40eb0a27a745d160a0e82e14b5a4155733eb7910
Yevhen999 <leshchenkoyevhen@gmail.com>
2022-11-07 16:30:22 +0000

    Deploying to gh-pages from @ Yevhen999/tasks@0f6d865ebf28bd2e06dd5a4fdf885cbc0554a4d5 🚀

---
+++ index.008aa166.js
@@ -0,0 +1,2 @@
+!function(n){const o={M:1e3,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};let t="";for(let C in o)for(;n>=o[C];)t+=C,n-=o[C]}(36);
+//# sourceMappingURL=index.008aa166.js.map
---
+++ index.ec741f33.js
@@ -0,0 +1,2 @@
+!function(r){var C={M:1e3,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};for(var X in C)for(;r>=C[X];)X,r-=C[X]}(36);
+//# sourceMappingURL=index.ec741f33.js.map
