ddab6de8529c4538723b1ff6f31868eb9a5598eb
Yevhen999 <leshchenkoyevhen@gmail.com>
2022-11-07 01:49:21 +0000

    Deploying to gh-pages from @ Yevhen999/tasks@2a1052db2ed90d74935bb95f955c9796b4f623ec 🚀

--- index.8bdaabe1.js
+++
@@ -1,2 +0,0 @@
-!function(){var I={2:"II",3:"III",4:"IV",5:"V",9:"IX",12:"XII",16:"XVI",29:"XXIX",36:"IIIVI",44:"XLIV",45:"XLV",68:"LXVIII",83:"LXXXIII",97:"XCVII",99:"XCIX",400:"CD",500:"D",501:"DI",649:"DCXLIX",798:"DCCXCVIII",891:"DCCCXCI",1e3:"M",1004:"MIV",1006:"MVI",1023:"MXXIII",2014:"MMXIV",3999:"MMMCMXCIX"};console.log(I[3999].toString())}();
-//# sourceMappingURL=index.8bdaabe1.js.map
